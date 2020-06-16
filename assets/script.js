f47b53829fbc5a042d188a07187b3e88d7c85bfb
Andrew Starr <astarr93@outlook.com>
2020-06-16 09:15:10 -0400

    Added logic to show questions and answers. Refinded timer

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,10 @@ let questionBank = [];
 const choiceDisplay = document.getElementById("choice-display");
 let answerBank = "";
 let answerCheck = "";
+let userSelection = "";
+let score = 0;
+let secondsLeft = 10;
+const timer = document.getElementsByClassName("timer");
 
 // Declared array containing all questions with their answers and correct answer.
 
@@ -25,7 +29,7 @@ const quizBank = [
   {
     question: "What was Straw Hat Luffy's first bounty?",
     answers: ["30,000 Beli", "15,000 Beli", "50,000 Beli", "20,000 Beli"],
-    correctAnswer: "30,000",
+    correctAnswer: "30,000 Beli",
   },
   {
     question: "What was the name of the devil fruit that Straw Hat Luffy ate?",
@@ -58,40 +62,76 @@ startButton.addEventListener("click", function () {
 function startQuiz() {
   showQuestions();
   showAnswers();
-  // startTimer();
+  startTimer();
   // checkAnswers();
   // showResults();
 }
 
 function showQuestions() {
   questionDisplay.innerText = quizBank[questionCounter].question;
-  console.log(questionBank);
 }
 
-function showAnswers() {}
-{
-  choiceDisplay.innerText = quizBank[questionCounter].answers;
+function showAnswers() {
+  choiceDisplay.innerText = "";
+  for (let i = 0; i < quizBank[questionCounter].answers.length; i++) {
+    let radioId = "answer" + i;
+    let radioInput = document.createElement("input");
+    radioInput.setAttribute("type", "radio");
+    radioInput.setAttribute("name", "answer");
+    radioInput.setAttribute("value", quizBank[questionCounter].answers[i]);
+    radioInput.setAttribute("id", radioId);
+    // radioInput.setAttribute(
+    //   "onclick",
+    //   "submitAnswer(" + quizBank[questionCounter].answers[i] + ")"
+    // );
+    let label = document.createElement("label");
+    label.setAttribute("for", radioId);
+    label.innerHTML = quizBank[questionCounter].answers[i];
+    choiceDisplay.appendChild(radioInput);
+    choiceDisplay.appendChild(label);
+  }
+  let submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  choiceDisplay.appendChild(submit);
+  // submit.setAttribute("onclick", checkAnswer());
+}
+choiceDisplay.addEventListener("submit", function (event) {
+  event.preventDefault();
+  for (let i = 0; i < this.elements.length; i++) {
+    if (this.elements[i].checked) {
+      if (this.elements[i].value === quizBank[questionCounter].correctAnswer) {
+        console.log("Correct");
+        secondsLeft += 10;
+      } else {
+        console.log("Wrong");
+        secondsLeft -= 10;
+      }
+    }
+  }
+  questionCounter++;
+  showQuestions();
+  showAnswers();
+});
+
+function submitAnswer(answer) {
+  console.log(answer);
 }
 
 // Timer Functions are below
 
-// function time() {
-//   secondsLeft--;
-//   if (secondsLeft <= 0) {
-//     secondsLeft = 0;
-//   }
-//   startTimer();
-// }
+function time() {
+  // startTimer();
+}
 
 // // Start Timer
 
-// function startTimer() {
-//   let timerInterval = setInterval(time, 1000);
-//   return timerInterval;
-// }
-
-// //STOP TIMER FUNCTION
-// function stopTimer() {
-//   clearInterval(timerReturn);
-//   timer.textContent = 0;
-// }
+function startTimer() {
+  let timerInterval = setInterval(function () {
+    secondsLeft -= 1;
+    timer.innerHTML = secondsLeft;
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+  // return timerInterval;
+}
