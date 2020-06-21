1f4c11af36609c5ff8f66d1e7c1a2fdc766b6772
Andrew Starr <astarr93@outlook.com>
2020-06-20 22:44:36 -0400

    Refactored JS logic for Q-A display, Timer, and score capture

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Declaring global variables to reference throughout the quiz
+// Declaring global variables
 
 const instructionsButton = document.getElementById("instructions");
 const startButton = document.getElementById("start");
@@ -8,14 +8,17 @@ let questionCounter = 0;
 let questionBank = [];
 const choiceDisplay = document.getElementById("choice-display");
 let answerBank = "";
-let answerCheck = 0;
+let answerCheck = "";
 let userSelection = "";
-let userName = "";
+let userInitials = "";
 let score = 0;
+let highScore = 0;
 let secondsLeft = 56;
-const timer = document.getElementById("title");
+const title = document.getElementById("title");
+let totalQuestionCount = "";
+let timerInterval = "";
 
-// Declared array containing all questions with their answers and correct answer.
+// Array containing all questions with their corresponding answers and correct answer
 
 const quizBank = [
   {
@@ -43,23 +46,91 @@ const quizBank = [
     ],
     correctAnswer: "Gomu Gomu no Mi",
   },
+  {
+    question: 'Who said: "Scars on the back are a swordsman\'s shame"?',
+    answers: [
+      "Dracule Mihawk",
+      "Roronoa Zoro",
+      "Trafalgar D. Water Law",
+      "Axe-Hand Morgan",
+    ],
+    correctAnswer: "Roronoa Zoro",
+  },
+  {
+    question:
+      "Luffy was faced with anger from which crew member when he made his annoucement about parting with the Going Merry",
+    answers: ["Chopper", "Sanji", "Nami", "Usopp"],
+    correctAnswer: "Usopp",
+  },
+  {
+    question: '"Iron Pirate" Franky had the original name of what?',
+    answers: ["Pandaman", "Mountain Ricky", "Cutty Flam", "Iceberg"],
+    correctAnswer: "Cutty Flam",
+  },
+  {
+    question:
+      "Who assisted Straw Hat Luffy in infiltrating the underwater prison Impel Down?",
+    answers: [
+      "Boa Hancock",
+      "Silvers Raleigh",
+      "Donquiote Doflamingo",
+      "Kizaru",
+    ],
+    correctAnswer: "Boa Hancock",
+  },
+  {
+    question:
+      'Pirate "Fire-Fist" Ace is the commander of which division of the Whitebeard Pirates?',
+    answers: ["1st", "2nd", "3rd", "4th"],
+    correctAnswer: "2nd",
+  },
+  {
+    question: "In the anime, Gol D. Roger was executed in which way?",
+    answers: [
+      "Stabbed with two spears",
+      "Hung from the gallows",
+      "Walked a plank",
+      "Decapitated",
+    ],
+    correctAnswer: "Stabbed with two spears",
+  },
+  {
+    question:
+      "Which member of the Straw Hat Pirates was the only one to have their bounty picture drawn?",
+    answers: ["Zoro", "Robin", "Brook", "Sanji"],
+    correctAnswer: "Sanji",
+  },
 ];
 
-// Instructions Button on-click event listener: Displays instructions in the question-display
+// Instructions Button on-click event listener: Displays quiz instructions to the user
+
 instructionsButton.addEventListener("click", function () {
-  questionDisplay.innerText = "Instructions:";
-  choiceDisplay.innerText =
-    "For every question you get right, you get +1 point and increase the timer by +10 seconds. If you answer wrong, the timer is decreased by 10 seconds. At the end of the quiz, the remaining time is added with your answer score to give your final score.";
+  title.innerText = "Instructions:";
+  questionDisplay.innerHTML =
+    "For every question you get right, you get +1 point and increase the timer by +10 seconds. If you answer wrong, the timer is decreased by 10 seconds." +
+    "<br />" +
+    "<br />" +
+    "At the end of the quiz, the remaining time is added with your answer score to give your final score.";
 });
 
-// Start Quiz Button on-click event listener: It starts the quiz
+// Start Quiz Button on-click event listener: This starts the quiz by calling one function to run multiple functions.
+
 startButton.addEventListener("click", function () {
   startQuiz();
 });
 
 // Leaderboards Button on-click event listener: Displays user's highscore stored in Chrome local storage in the question-display
+
 leaderBoardButton.addEventListener("click", function () {
-  questionDisplay.innerText = "Your highest score is";
+  title.innerText = "Leaderboard:";
+  choiceDisplay.innerText = "";
+  if (userInitials != "") {
+    questionDisplay.innerText =
+      "The high score belongs to: " + userInitials + "" + highScore;
+  } else {
+    questionDisplay.innerText =
+      "No high score recorded yet. Click Start Quiz and give it your all!";
+  }
 });
 
 // These functions make up the abstract steps of the quiz
@@ -67,10 +138,7 @@ leaderBoardButton.addEventListener("click", function () {
 function startQuiz() {
   hideMenu();
   showQuestions();
-  showAnswers();
   startTimer();
-  // checkAnswers();
-  // showResults();
 }
 
 // Hide Menu function: Hides Leaderboards and Start Quiz Button during quiz
@@ -84,91 +152,103 @@ function hideMenu() {
 // Show Menu function: Shows Leaderboards and Start Quiz Button when not taking quiz.
 
 function showMenu() {
-  startButton.style.visibility = "display";
-  leaderBoardButton.style.visibility = "display";
-  instructionsButton.style.visibility = "display";
+  startButton.style.visibility = "visible";
+  leaderBoardButton.style.visibility = "visible";
+  instructionsButton.style.visibility = "visible";
 }
 
 // Show Questions function: Displays questions on the document
 
 function showQuestions() {
-  questionDisplay.innerText = quizBank[questionCounter].question;
+  if (quizBank.length == questionCounter) {
+    endQuiz();
+  } else {
+    questionDisplay.innerText = quizBank[questionCounter].question;
+    showAnswers();
+  }
 }
 
-// Show Answers function: Displays radio buttons as possible answer choices corresponding to the question from Quiz Bank.
+// Show Answers function: Displays answer choices and answer submit button
 
 function showAnswers() {
   choiceDisplay.innerText = "";
   for (let i = 0; i < quizBank[questionCounter].answers.length; i++) {
-    let radioId = "answer" + i;
-    let radioInput = document.createElement("input");
-    radioInput.setAttribute("type", "radio");
-    radioInput.setAttribute("name", "answer");
-    radioInput.setAttribute("value", quizBank[questionCounter].answers[i]);
-    radioInput.setAttribute("id", radioId);
+    let buttonInput = document.createElement("input");
+    buttonInput.setAttribute("type", "radio");
+    buttonInput.setAttribute("name", "answer");
+    buttonInput.setAttribute("value", quizBank[questionCounter].answers[i]);
+    buttonInput.setAttribute("id", "radioId");
     let label = document.createElement("label");
-    label.setAttribute("for", radioId);
+    label.setAttribute("for", "radioId");
     label.innerHTML = quizBank[questionCounter].answers[i];
-    choiceDisplay.appendChild(radioInput);
+    choiceDisplay.appendChild(buttonInput);
     choiceDisplay.appendChild(label);
   }
-  let submit = document.createElement("input");
-  submit.setAttribute("type", "submit");
-  choiceDisplay.appendChild(submit);
+  let submitButton = document.createElement("input");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("id", "submitId");
+  choiceDisplay.appendChild(submitButton);
+  submitButton.addEventListener("click", function () {
+    let answers = document.getElementsByName("answer");
+    answers.forEach(function (answer) {
+      if (answer.checked == true) {
+        userSelection = answer.value;
+        checkAnswers();
+      }
+    });
+  });
 }
 
-choiceDisplay.addEventListener("submit", function (event) {
-  event.preventDefault();
-  for (let i = 0; i < this.elements.length; i++) {
-    if (this.elements[i].checked) {
-      if (this.elements[i].value === quizBank[questionCounter].correctAnswer) {
-        console.log("Correct");
-        secondsLeft += 10;
-      } else {
-        console.log("Wrong");
-        secondsLeft -= 10;
-      }
-    }
+// Choice Display on Click event listener: After the user locks in their choice, we check for right/wrong and adjust score accordingly
+
+function checkAnswers() {
+  if (userSelection == quizBank[questionCounter].correctAnswer) {
+    score++;
+    secondsLeft += 10;
+  } else {
+    secondsLeft -= 10;
   }
   questionCounter++;
   showQuestions();
-  showAnswers();
-});
+}
 
-// function submitAnswer(answer) {
-//   console.log(answer);
-// }
+// End Quiz function: This function controls the handling of end of quiz and prompts users to save their initials and score to local storage - referenced in Leaderboard button
 
 function endQuiz() {
-  let userInitials = prompt("Enter your name: ");
-  localStorage.setItem("Username", "userInitials");
+  clearInterval(timerInterval);
+  title.innerText = "Thanks for playing!";
+  showMenu();
+  highScore = score + secondsLeft;
+  questionDisplay.innerHTML =
+    "You got " +
+    score +
+    " out of " +
+    quizBank.length +
+    " right for a final score of: " +
+    highScore;
+  userInitials = prompt("Enter your initials: ");
+  localStorage.setItem("Username", userInitials);
+  localStorage.setItem("Highscore", highScore);
 }
-// Timer Function: This function shows the timer, initiates a countdown using a global variable, and ends the quiz at 0
+
+// Timer Functions and Logic:
+// Start Timer Function: Displays timer, initiates countdown from global variable
 
 function startTimer() {
-  let timerInterval = setInterval(function () {
-    timer.innerText = "Remaining Time: " + secondsLeft;
+  timerInterval = setInterval(function () {
+    title.innerText = "Remaining Time: " + secondsLeft;
     secondsLeft -= 1;
-    timer.innerText = "Remaining Time: " + secondsLeft;
+    title.innerText = "Remaining Time: " + secondsLeft;
     if (secondsLeft <= 10) {
-      timer.setAttribute("class", "final-countdown");
+      title.setAttribute("class", "final-countdown");
     }
-    while (secondsLeft <= 0) {
-      timer.innerText = "Game Over";
-      break;
+    if (secondsLeft <= 0) {
+      title.innerText = "Game Over";
+      endQuiz();
     }
   }, 1000);
 }
 
-// When the timer reaches 0, run endQuiz()
-
-if (secondsLeft <= 0) {
-  endQuiz();
-}
-
-// Record Results Function: This function prompts users to store their score
-
-function recordResults() {
-  userName = prompt("Record your name: ");
-  localStorage(userName);
+function stopTimer() {
+  clearInterval(timerInterval);
 }
